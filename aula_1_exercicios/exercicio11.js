dffc0589e55f3d434d3f54764c93dd2758a21850
Braian-de-Liz <delizbraian@gmail.com>
2025-08-27 20:05:48 -0300

    exercicio 11

---
+++ aula_1_exercicios/exercicio11.js
@@ -0,0 +1,5 @@
+import prompt from "prompt";
+
+const prompt = prompt();
+
+
---
+++ aula_1_exercicios/exercicio11.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10 - Números pares entre</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="numero1">Digite o primeiro número:</label>
+        <input type="number" id="numero1" name="numero1" required>
+
+        <br>
+
+        
+        <button type="submit" name="fibonacci">gerar fibonacci</button>
+    </form>
+    <?php
+    
+    ?>
+</body>
+</html>
